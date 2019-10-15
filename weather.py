f46d74ae7ceb067cc71a35d3b3cbfd215bad9553
Marouf Arnaout <marnaout96@gmail.com>
2019-10-16 00:08:37 +0300

    Resampled data to be weekly and added range bars

--- weather.py
+++ weather.py
@@ -1,17 +1,19 @@
+import numpy as np
 import pandas as pd
 import plotly.express as px
 
-data = pd.read_csv('beirut.csv')
 
-years = []
-dates = []
-for index, row in data.iterrows():
-	date = row['date_time'].split('-')
-	years.append(date[0])
-	dates.append('-'.join((['2019'] + date[1:])))
-
-data['year'] = years
-data['date'] = dates
-
-plot = px.line(data, x='date', y='tempC', color='year', line_group='year', filename='data.html')
+d = pd.read_csv('beirut.csv', parse_dates=['date_time'])
+d = d.set_index('date_time')
+data = pd.DataFrame()
+r = d.tempC.resample('W')
+data['tempC'] = r.mean()
+data['minTempC'] = r.min()
+data['maxTempC'] = r.max()
+data['range_max'] = data['maxTempC'] - data['tempC']
+data['range_min'] = data['tempC'] - data['minTempC']
+data['year'] = data.index.map(lambda x: x.year)
+data['date'] = data.index.map(lambda x: x.replace(year=2019))
+print(data)
+plot = px.line(data, x='date', y='tempC', color='year', line_group='year', error_y='range_max', error_y_minus='range_min')
 plot.show()
